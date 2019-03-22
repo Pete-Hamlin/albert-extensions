8920c6fb09b7c995922c0491c35b7d3e51fc9b2c
Pete Hamlin <pete.hamlin1@gmail.com>
2019-03-22 13:41:35 +0000

    Cleanup & added clip function

--- src/Pocket/__init__.py
+++ src/Pocket/__init__.py
@@ -34,42 +34,38 @@ def handleQuery(query):
         if query.string in item['given_url']:
             debug('Matched URL {}'.format(item['given_url']))
             items.append(append_item(item))
+            continue
         # Match authors
         if 'authors' in item:
             for key, author in item['authors'].items():
                 if query.string in author['name']:
-                    info('Matched author {}'.format(author['name']))
+                    debug('Matched author {}'.format(author['name']))
                     items.append(append_item(item))
+                    continue
         # Match tags
         if 'tags' in item:
             for key, tag in item['tags'].items():
                 if query.string in tag['tag']:
-                    info('Matched tag {}'.format(tag['tag']))
+                    debug('Matched tag {}'.format(tag['tag']))
                     items.append(append_item(item))
+                    continue
 
     item = Item(id=__prettyname__,
                 icon=__icon__,
                 text="(Re)Authenticate",
                 subtext="Attempt to (re)generate auth token from Pocket API",
-                completion=__trigger__ + 'Hellooohooo!',
+                completion=__trigger__,
                 urgency=ItemBase.Alert,
                 actions=[
                     FuncAction(text="FuncAction",
-                               callable=lambda: authenticate()),
-                    UrlAction(text="UrlAction",
-                               url="https://www.google.de"),
-                    ProcAction(text="ProcAction",
-                               commandline=["espeak", "hello"],
-                               cwd="~"),  # optional
-                    TermAction(text="TermAction",
-                               commandline=["sleep", "5"],
-                               cwd="~/git")  # optional
-                ])
+                               callable=lambda: authenticate())
+                                ])
     items.append(item)
     return items
 
 def append_item(value):
-    actions = [UrlAction("Open in browser", value['given_url'])]
+    actions = [UrlAction("Open in browser", value['given_url']),
+               ClipAction("Copy to clipboard", value['given_url'])]
     subtext = "{}...".format(value['given_url'][:30])
     if 'given_title' in value and value['given_title']:
         text = value['given_title'][:60]
@@ -82,7 +78,6 @@ def append_item(value):
         for key, tag in value['tags'].items():
             subtext = subtext + " | {}".format(tag['tag'])
     item = Item(id=__prettyname__,
-                icon=__icon__,
                 text=text,
                 subtext=subtext,
                 actions=actions
