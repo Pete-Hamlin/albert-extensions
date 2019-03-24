b0022a2bcc9008d114e390e591cc232f85d9266d
Pete Hamlin <pete.hamlin1@gmail.com>
2019-03-24 18:40:43 +0000

    Updated install script

--- install-all.sh
+++ install-all.sh
@@ -1,6 +1,13 @@
-#! /bin/sh
+#! /bin/bash
 # Lazy install script - run from within this repository
 
-DIR=$HOME/.local/share/albert/org.albert.extension.python/modules/
-
+if [ "$EUID" -ne 0 ]
+    then echo Installing as user...
+    DIR=$HOME/.local/share/albert/org.albert.extension.python/modules/
+else
+    echo Installing as root...
+    DIR=/usr/share/albert/org.albert.extension.python/modules
+fi
+echo Installing extensions into $DIR
 cp -r **/src/* $DIR
+echo Done!
